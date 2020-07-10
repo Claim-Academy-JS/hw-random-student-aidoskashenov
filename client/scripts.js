2b68d9dce24c1046e21b08d87f1a149fc72f8eac
Aidos <aidos_ka@yahoo.com>
2020-07-10 12:38:22 -0500

    Add Radnom Feature

--- client/scripts.js
+++ client/scripts.js
@@ -1,29 +1,41 @@
 const students = []
-
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min // The maximum is inclusive and the minimum is inclusive
+}
 document.querySelector('form').addEventListener('submit', function () {
   event.preventDefault()
-  var addedStudent = 0
-  addedStudent = this.parentElement.querySelector('#name').value
-  students.push(addedStudent)
-  console.log(students)
 
+  const addedStudent = this.parentElement.querySelector('#name').value
+
+  const existingStudent = students.find(
+    (currentStudentInArray) => currentStudentInArray === addedStudent)
+
+  if (addedStudent !== existingStudent) {
+    students.push(addedStudent)
+  }
+  console.log(students)
 })
 
+document.querySelector('#button1').addEventListener('click', function () {
+  event.preventDefault()
 
-
-
-
+  console.log(students[getRandomIntInclusive(0, students.length - 1)])
+}
+)
 
 /**
  * Getting a random integer between two values, inclusive (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
- * @param {number} min 
- * @param {number} max 
+ * @param {number} min
+ * @param {number} max
  * @returns {number}
- */
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive
-}
+ console.log(getRandomIntInclusive(0, students.length-1))
+
+  function getRandomIntInclusive (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min // The maximum is inclusive and the minimum is inclusive
+  } */
 
 // TODO: Upon clicking your 'giant button', use 'getRandomIntInclusive' to randomly pick a student from your Array. You can just log that for now.
